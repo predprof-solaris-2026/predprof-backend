080e10b20d009f3c804fd8e60366854c02fdb99e
Anisa6 <anna.kr.12@mail.ru>
2026-02-16 00:49:46 +0300

    block user and change role

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Depends
-from typing import Annotated, Dict
+from fastapi import APIRouter, Depends, status
+from typing import Annotated, Dict, Literal
 from app.data import schemas
 from app.data.models import User, Admin
 from app.utils.auth import create_user, authenticate_user
 from app.utils.security import verify_password
 from app.utils.exceptions import Error
-from app.utils.security import verify_password, get_current_user
+from app.utils.security import verify_password, get_current_user, get_current_admin
 from datetime import timedelta
 from fastapi.security import OAuth2PasswordRequestForm
 
@@ -87,3 +87,55 @@ async def get_all_users() -> list[schemas.UserResponse]:
         )
         for user in users
     ]
+
+
+@router.put("/{user_id}/block", status_code=status.HTTP_204_NO_CONTENT)
+async def block_user(user_id: str, current_user: Admin = Depends(get_current_admin)):
+    admin = await Admin.find_one(Admin.email == current_user.email)
+    print(admin, current_user.email)
+    if not admin:
+        raise Error.NOT_ADMIN
+    
+    user = await User.get(user_id)
+    if not user:
+        raise Error.NOT_FOUND
+    
+    user.is_blocked = True
+    await user.save()
+
+
+@router.put("/{user_id}/role", status_code=status.HTTP_204_NO_CONTENT)
+async def change_user_role(user_id: str, new_role: Literal["user", "admin"], current_user: Admin = Depends(get_current_admin)):
+    admin = await Admin.find_one(Admin.email == current_user.email)
+    if not admin:
+        raise Error.NOT_ADMIN
+    
+    if new_role == "admin":
+        user = await User.get(user_id)
+        if not user:
+            raise Error.NOT_FOUND
+        
+        new_admin = Admin(
+            first_name=user.first_name,
+            last_name=user.last_name,
+            email=user.email,
+            password_hash=user.password_hash
+        )
+        await new_admin.insert()
+        await user.delete()
+    
+    elif new_role == "user":
+        admin_to_convert = await Admin.find_one(Admin.id == user_id)
+        if not admin_to_convert:
+            raise Error.NOT_FOUND
+        
+        new_user = User(
+            first_name=admin_to_convert.first_name,
+            last_name=admin_to_convert.last_name,
+            email=admin_to_convert.email,
+            password_hash=admin_to_convert.password_hash,
+            is_blocked=False,
+            elo_rating=1000
+        )
+        await new_user.insert()
+        await admin_to_convert.delete() 
